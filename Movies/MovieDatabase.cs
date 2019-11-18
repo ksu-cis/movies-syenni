905a4e7bb65e024ae3962436df4f4e79ef51214e
Seth Yenni <sethyenni@users.campus>
2019-11-18 15:05:46 -0600

    In class work

--- Movies/MovieDatabase.cs
+++ Movies/MovieDatabase.cs
@@ -27,5 +27,45 @@ namespace Movies
         }
 
         public List<Movie> All { get { return movies; } }
+
+        public List<Movie> SearchAndFilter(string searchString, List<string> rating)
+        {
+            // Case 0: No search string or ratings
+            if (searchString == null) return All;
+
+            List<Movie> results = new List<Movie>();
+            foreach(Movie movie in movies)
+            {
+                // Case 1: Serach string AND ratings
+                if (searchString != null)
+                {
+                    if (movie.Title != null && movie.Title.Contains(searchString, StringComparison.InvariantCultureIgnoreCase) && rating.Contains(movie.MPAA_Rating))
+                    {
+                        results.Add(movie);
+                    }
+
+                }
+
+                // Case 2: Search string only
+                else if (rating.Count == 0)
+                {
+                    {
+                        if (movie.Title != null && movie.Title.Contains(searchString))
+                        {
+                            results.Add(movie);
+                        }
+                    }
+                }
+                // Case 3: Ratings only
+                else if (rating.Count > 0)
+                {
+                    if (rating.Contains(movie.MPAA_Rating))
+                    {
+                        results.Add(movie);
+                    }
+                }
+            }
+            return results;
+        }
     }
 }
